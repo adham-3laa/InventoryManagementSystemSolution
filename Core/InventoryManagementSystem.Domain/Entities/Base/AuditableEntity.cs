a33a55f250dcd0fe7c536e47854a03969f1ee9da
adham alaa <alaam2845@gmail.com>
2026-04-20 20:20:05 +0200

    Add project files.

---
+++ Core/InventoryManagementSystem.Domain/Entities/Base/AuditableEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Domain.Entities.Base
+{
+    public abstract class AuditableEntity<TKey> : BaseEntity<TKey>
+    {
+        public DateTime CreatedOn { get; set; }
+        public DateTime? LastModifiedOn { get; set; }
+    }
+}
---
+++ Core/InventoryManagementSystem.Domain/Entities/Base/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Domain.Entities.Base
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; } = default!;
+    }
+}
