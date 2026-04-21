36f427a501f493a7c0a6e802e028d61789a28b05
adham alaa <alaam2845@gmail.com>
2026-04-21 15:30:47 +0200

    UOW

---
+++ Core/InventoryManagementSystem.Domain/Contracts/UOW/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using InventoryManagementSystem.Domain.Contracts.Repos;
+using InventoryManagementSystem.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Domain.Contracts.UOW
+{
+    public interface IUnitOfWork
+    {
+        IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity:BaseEntity<TKey>;
+        Task<int> SaveChangesAsync();
+    }
+}
---
+++ Infrastructure/InventoryManagementSystem.Persistence/UOW/UnitOfWork.cs
@@ -0,0 +1,38 @@
+﻿using InventoryManagementSystem.Domain.Contracts.Repos;
+using InventoryManagementSystem.Domain.Contracts.UOW;
+using InventoryManagementSystem.Domain.Entities.Base;
+using InventoryManagementSystem.Persistence.Context;
+using InventoryManagementSystem.Persistence.Repos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Persistence.UOW
+{
+    public class UnitOfWork(InventoryDbContext context) : IUnitOfWork
+    {
+       
+        private readonly Dictionary<Type, object> _repos = new();
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>()
+            where TEntity : BaseEntity<TKey>
+        {
+            var entityType = typeof(TEntity);
+
+            if (!_repos.TryGetValue(entityType, out var repo))
+            {
+                repo = new GenericRepository<TEntity, TKey>(context);
+                _repos[entityType] = repo;
+            }
+
+            return (IGenericRepository<TEntity, TKey>)repo;
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await context.SaveChangesAsync();
+        }
+        
+        
+    }
+}
