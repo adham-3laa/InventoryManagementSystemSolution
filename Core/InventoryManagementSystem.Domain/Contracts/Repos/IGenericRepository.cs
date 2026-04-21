261d5e4a3bbca7f807d4fec442f5959e92045157
adham alaa <alaam2845@gmail.com>
2026-04-21 13:54:47 +0200

    Contracts.Repos

---
+++ Core/InventoryManagementSystem.Domain/Contracts/Repos/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using InventoryManagementSystem.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InventoryManagementSystem.Domain.Contracts.Repos
+{
+    public interface IGenericRepository <TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
